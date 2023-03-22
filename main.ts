8fef57379ba4f942d45f504799029ca0fc1230e9
Kid-U <123192306+Kid-U@users.noreply.github.com>
2023-03-22 09:05:51 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,21 +1,3 @@
-let mySprite = sprites.create(img`
-    . . . . f f f f f . . . . . . . 
-    . . . f e e e e e f . . . . . . 
-    . . f d d d d e e e f f . . . . 
-    . c d d d d d d e e d d f . . . 
-    . c d f d d f d e e b d c . . . 
-    c d d f d d f d e e b d c . f f 
-    c d e e d d d d e e f c . f e f 
-    c d d d d c d e e e f . . f e f 
-    . f c c c d e e e f f . . f e f 
-    . . f f f f f e e e e f . f e f 
-    . . . . f e e e e e e e f f f . 
-    . . f f e f e e f e e e e f . . 
-    . f e f f e e f f f e e e f . . 
-    f d d b d d c f f f f f f b f . 
-    f d d c d d d f . . f c d d f . 
-    . f f f f f f f . . . f f f . . 
-    `, SpriteKind.Player)
 let snake = sprites.create(img`
     . . . . c c c c c c . . . . . . 
     . . . c 6 7 7 7 7 6 c . . . . . 
